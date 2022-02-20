797da6ff41ae2619d33398e704bc6a9333a13757
Christian Medina <cmedina@bepsa.com.py>
2022-02-19 22:44:58 -0300

    updated readme file, added diagram images, created a new file with the OpenApi specification

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 - [Contenido.](#contenido)
   - [Introduccion.](#introduccion)
   - [Tecnologias.](#tecnologias)
-  - [](#)
+  - [Requerimientos.](#requerimientos)
+  - [Diagramas.](#diagramas)
 
 ## Introduccion.
 
@@ -18,12 +19,47 @@ Luego los datos en la base de datos Mongodb seran utilizados para crear un datas
 final sera utilizado para alimentar un modelo de machine learning.
 
 ## Tecnologias.
-- Django y Django Rest, es un framework para el desarrollo de API, esta escrito en Python.
-- Postgresql, para guardar los datos de los usuarios y metada en general.
+- Docker, contenedor de aplicaciones, todos los servicios estan conectados y trabajan en conjunto gracias a la utilizacion de docker-compose.
+- Django y Django Rest, es un framework para el desarrollo de APIs, esta escrito en Python.
+- Postgresql, para guardar los datos de los usuarios y la metada en general.
 - Mongodb, base de datos no relacional, sera la responsablde de guardar los dataframes que seran utilizados para alimentar el modelo de machine learning.
-- Redis, motor de base de datos en memoria, sera la encargada de cachear ciertas peticiones a nuestro API.
+- Redis, motor de base de datos en memoria, sera la encargada de cachear ciertas peticiones de nuestro servicio.
 - Celery, es un distribuidor asincrono de tareas.
 - Rabbitmq, como broker de mensajes, trabaja en conjunto con celery.
 - Pandas, un excel en esteroides.
 
-## 
+## Requerimientos.
+Estos son unos requerimientos ficticios, utilizados para poder implementar varias tecologias en las que estoy interesado.
+
+Una empresa de retail necesita un servicio que pueda realizar un cluster de clientes de acuerdo a habitos de compras, y necesita visualizarlo a traves de un dashboard, para poder armar campañas de marketing mas efectivas.
+
+Teniendo en cuenta los requerimientos del negocio, el servicio debe de manejar el registro de usuarios y clientes. Cada cliente podra tener mas de una sucursal. 
+El cliente cargara el archivo csv a travez de un servcio que se encargara de extraer la data, generar un dataframe y almacenar; la data extraida en un base de datos, y el archivo csv en un sistema de archivos.
+El siguiente paso consiste en procesar dichos dataframes a travez de un modelo de machine learning y el resultado guardarlo en una base de datos para luego generar infomes del mismo.
+ 
+
+## Diagramas.
+En la siguiente imagene podemos ver el flujo cuando el usuario carga un archivo csv.
+
+![Usuario carga archivo csv](diagram-images/user-save-csv.png)
+
+  1. El usuario carga el archivo csv a travez del servicio.
+     1. El servicio genera un hash por cada archivo que recibe, y lo guarda como metadata, luego compara si dicho hash ya existe en la base de datos, de ser asi, envia un error diciendo que el archivo ya fue alzado.
+  2. Se guarda metadatos del archivo y el usuario.
+  3. Guarda el archivo csv en un sistema de archivos.
+  4. Celery crea un trabajo asincrono y lo pasa al broker.
+  5. El worker verifica si existen trabajos en el broker.
+  6. El worker genera un dataframe a partir de los datos dentro del archivo csv, dicho dataframe es guardado en la base de datos mongo, y mas metadata es agregada a la base de datos postgres.
+
+A continuacion una descripcion del flujo cuando el usuario solicita un informe.
+
+![Usuario solicita informe](diagram-images/user-request-new-report.png)
+
+  1. El usuario solicita un informe.
+  2. La API verifica si los datos se encuentran cacheados.
+  3. A traves de celery se crea un nuevo job que es enviado al broker.
+  4. El worker toma un nuevo trabajo.
+  5. Verifica si existen datos del cliente en la base de datos postgresql, de ser asi, extrae sus datos y metadatos.
+  6. Obtiene los dataframes correspondiente a ese usuario.
+  7. Analiza los dataframes con un modelo de machine learning dedicado al clustering. 
+  8. Guarda la informacion en postgresql y notifica al usuario.
--- data/logic/customer_input.py
+++ data/logic/customer_input.py
@@ -1,4 +1,5 @@
 import logging
+from re import A
 import uuid
 import json
 from django.conf import settings
@@ -11,6 +12,7 @@ from data.utils.constant import Status
 from data.utils.exceptions import DuplicatedRecord, EntityNotFound
 from data.utils.upload_file import upload_file_to_local
 from data.task.merge_dataframes import merge_dataframes
+
 
 class CustomerInputLogic():
     def __init__(self):
@@ -75,35 +77,47 @@ class CustomerInputLogic():
         customer_queryset = models.CustomerInput.objects.get_all_by_customer_code(
             customer_code)
         if customer_queryset:
-            customer_gridfs_list = []
-            for customer in customer_queryset:
-                customer_gridfs = customer.gridfs_code
-                customer_gridfs_list.append(customer_gridfs)
-            customer_json_list = []
-            for gridfs in customer_gridfs_list:
-                customer_aux = self.__get_customer_json_by_gridfs_code(customer_code, gridfs)
-                customer_json_list.append(customer_aux)
-            customer_json = {}
-            for cust in customer_json_list:
-                customer_json = merge_dataframes(cust, customer_json)    
-
-            customer_gridfs_code = customer_queryset[0].gridfs_code
-            # test_collection = TestCollection()
-            # customer_json = test_collection.list_customer_input_with_gridfs(
+            if len(customer_queryset) == 1:
+                gridfs = customer_queryset[0].gridfs_code
+                return self.__convert_from_json_to_dict(self.__get_customer_json_by_gridfs_code(customer_code, gridfs))
+            elif len(customer_queryset) >= 2:
+                customer_gridfs_list = []
+                for customer in customer_queryset:
+                    customer_gridfs = customer.gridfs_code
+                    customer_gridfs_list.append(customer_gridfs)
+                customer_json_list = []
+                for gridfs in customer_gridfs_list:
+                    customer_aux = self.__get_customer_json_by_gridfs_code(
+                        customer_code, gridfs)
+                    customer_json_list.append(customer_aux)
+                customer_json = customer_json_list[0]
+                for x in range(len(customer_json_list)):
+                    if x+1 < len(customer_json_list):
+                        customer_json = merge_dataframes(
+                            customer_json, customer_json_list[x+1])
+                    else:
+                        break
+                aux = self.__convert_from_json_to_dict(customer_json)
+                print("#"*50)
+                print(len(aux))
+                return aux
+                # customer_gridfs_code = customer_queryset[0].gridfs_code
+                # test_collection = TestCollection()
+                # customer_json = test_collection.list_customer_input_with_gridfs(
                 # customer_code, customer_gridfs_code)
-            # convertimos la coleccion de tipo json a una lista de diccionarios,
-            # para su mejor representacion en la view.
-            customer_list = []
-            customer_columns = customer_json["columns"]
-            customer_index = customer_json["index"]
-            customer_data = customer_json["data"]
-            for i in range(len(customer_index)):
-                customer = {}
-                for x in range(len(customer_columns)):
-                    customer.update({customer_columns[x]: customer_data[i][x]})
-                customer.update({"index": customer_index[i]})
-                customer_list.append(customer)
-            return customer_list
+                # convertimos la coleccion de tipo json a una lista de diccionarios,
+                # para su mejor representacion en la view.
+                # customer_list = []
+                # customer_columns = customer_json["columns"]
+                # customer_index = customer_json["index"]
+                # customer_data = customer_json["data"]
+                # for i in range(len(customer_index)):
+                #     customer = {}
+                #     for x in range(len(customer_columns)):
+                #         customer.update({customer_columns[x]: customer_data[i][x]})
+                #     customer.update({"index": customer_index[i]})
+                #     customer_list.append(customer)
+                # return customer_list
         else:
             raise EntityNotFound(
                 f"No customer data with customer_code: {customer_code}"
@@ -125,3 +139,16 @@ class CustomerInputLogic():
         customer_json = test_collection.list_customer_input_with_gridfs(
             customer_code, gridfs_code)
         return customer_json
+
+    def __convert_from_json_to_dict(self, customer_json):
+        customer_columns = customer_json["columns"]
+        customer_index = customer_json["index"]
+        customer_data = customer_json["data"]
+        customer_list = []
+        for i in range(len(customer_index)):
+            customer = {}
+            for x in range(len(customer_columns)):
+                customer.update({customer_columns[x]: customer_data[i][x]})
+                customer.update({"index": customer_index[i]})
+                customer_list.append(customer)
+        return customer_list
