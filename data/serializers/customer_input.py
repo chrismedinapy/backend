9a941c0189501bafe6f239056bc6bb907c7d1d1a
chris <chrismedinapy@gmail.com>
2022-06-17 14:57:30 -0400

    fixed bug on function create of the class CustomerInputViewClass

--- data/serializers/customer_input.py
+++ data/serializers/customer_input.py
@@ -3,3 +3,7 @@ from rest_framework import serializers
 
 class CustomerInputSerializers(serializers.Serializer):
     customer_input_description = serializers.CharField(max_length=1000)
+
+
+class CustomerPayloadSerializers(serializers.Serializer):
+    customer_payload = serializers.DictField()
--- data/views/customer_input.py
+++ data/views/customer_input.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 from data.utils.paginator import CustomPageNumberPaginator
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
@@ -7,7 +8,10 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
 from data.logic.customer_input import CustomerInputLogic
-from data.serializers.customer_input import CustomerInputSerializers
+from data.serializers.customer_input import (
+    CustomerInputSerializers,
+    CustomerPayloadSerializers,
+)
 from data.utils.exceptions import InvalidParameter
 from data.utils.validator import body_validator, csv_validator, uuid_validator
 
@@ -19,26 +23,30 @@ class CustomerInputViewClass(ViewSet):
         uuid_validator(customer_code)
         uuid_validator(retail_store_code)
         customer_payload = request.data.get("customer")
-        if not customer_payload:
-            raise InvalidParameter("Field 'customer' it's required")
-        customer = json.loads(customer_payload)
+        try:
+            customer = json.loads(customer_payload)
+        except json.decoder.JSONDecodeError:
+            raise InvalidParameter("Customer must be a valid JSON")
         body_validator(customer, CustomerInputSerializers)
-        
         customer_csv = request.data.get("customer_csv")
         if not customer_csv:
             raise InvalidParameter("Field 'customer_csv' it's required")
         csv_validator(customer_csv)
 
-        self.customer_input_logic.create(customer, customer_csv, customer_code, retail_store_code)
+        self.customer_input_logic.create(
+            customer, customer_csv, customer_code, retail_store_code
+        )
 
         return Response(status=status.HTTP_201_CREATED)
 
-    @method_decorator(cache_page(60*60*2))
+    @method_decorator(cache_page(60 * 60 * 2))
     @method_decorator(vary_on_cookie)
     def list(self, request, customer_code, retail_store_code):
         pag = CustomPageNumberPaginator()
         uuid_validator(retail_store_code)
         uuid_validator(customer_code)
-        customer_input_json = self.customer_input_logic.list(customer_code, retail_store_code)
+        customer_input_json = self.customer_input_logic.list(
+            customer_code, retail_store_code
+        )
         results = pag.paginate_queryset(customer_input_json, request)
         return pag.get_paginated_response(results)
