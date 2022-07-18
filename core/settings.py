1892b1eee51e6bc33638c066c3cbd7373924f77f
Tobias Jara <tjarahire@gmail.com>
2022-07-18 15:09:13 -0400

    MPY, added cors requirement

--- core/settings.py
+++ core/settings.py
@@ -49,9 +49,11 @@ INSTALLED_APPS = [
     'middleware',
     'django.contrib.gis',
     'rest_framework_gis',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -194,3 +196,8 @@ RABBITMQ_DEFAULT_VHOST = config("RABBITMQ_DEFAULT_VHOST")
 
 # CELERY_BROKER_URL = f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}/{RABBITMQ_DEFAULT_VHOST}"
 CELERY_BROKER_URL = f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}/{RABBITMQ_DEFAULT_VHOST}"
+
+CORS_ORIGIN_ALLOW_ALL = True
+""" CORS_ORIGIN_WHITELIST = (
+  'http://localhost:3000',
+) """
