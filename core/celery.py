a33788490d2129dd9b95259bd0703c123879796d
Chris <70601548+chrismedinapy@users.noreply.github.com>
2026-07-13 10:07:48 -0300

    Avoid eager Redis config loading in Celery

--- core/celery.py
+++ core/celery.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-from decouple import config
 
 import os
 
@@ -8,9 +7,5 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("core")
-REDIS_PASSWORD=config("REDIS_PASSWORD")
-REDIS_HOST=config("REDIS_HOST")
-REDIS_PORT=config("REDIS_PORT")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-# app.conf.broker_url=CELERY_BROKER_URL=f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0'
 app.autodiscover_tasks()
