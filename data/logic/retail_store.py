aa8e0ebe31301634e086228161fb9f030ca92663
Chris <70601548+chrismedinapy@users.noreply.github.com>
2026-07-15 18:17:45 -0300

    fix: accept validated retail store Point values

--- data/logic/retail_store.py
+++ data/logic/retail_store.py
@@ -2,7 +2,8 @@ import uuid
 
 from django.contrib.gis.geos import Point
 from django.forms import model_to_dict
-from data.models.retail_store import RetailStore, Customer
+
+from data.models.retail_store import Customer, RetailStore
 from data.utils.constant import Status
 from data.utils.exceptions import EntityNotFound, InvalidParameter
 
@@ -14,15 +15,10 @@ class RetailStoreLogic:
     def create(self, retail_store_data, customer_code):
         retail_store_code = str(uuid.uuid4())
         retail_store_name = retail_store_data.get("retail_store_name")
-        retail_store_location = retail_store_data.get("retail_store_location")
-        if (
-            not retail_store_location["latitude"]
-            or not retail_store_location["longitude"]
-        ):
-            raise InvalidParameter(f"Missing longitude or latitude fields.")
-        point = Point(
-            retail_store_location["latitude"], retail_store_location["longitude"]
-        )
+        point = retail_store_data.get("retail_store_location")
+        if not isinstance(point, Point) or point.empty:
+            raise InvalidParameter("A valid retail_store_location is required.")
+
         retail_store_city = retail_store_data.get("retail_store_city")
         retail_store_status = Status.ACTIVE.value
 
@@ -37,12 +33,12 @@ class RetailStoreLogic:
             status=retail_store_status,
             customer=customer,
         )
-        new_retail_saved = RetailStore.objects.save(new_retail_store)
+        RetailStore.objects.save(new_retail_store)
 
     def get_all(self, customer_code):
         retail_stores = RetailStore.objects.get_all_by_customer_code(customer_code)
         if not retail_stores:
-            raise EntityNotFound(f"There are no Retail store.")
+            raise EntityNotFound("There are no Retail store.")
         retail_store_list = []
         for retail in retail_stores:
             retail_store_list.append(self.__retail_store_mapped(retail))
@@ -66,7 +62,7 @@ class RetailStoreLogic:
         point = retail_store.retail_store_location
         retail_store_dict["retail_store_code"] = str(retail_store.retail_store_code)
         retail_store_dict["retail_store_location"] = {
-            "latitude": point.coords[0],
-            "longitude": point.coords[1],
+            "latitude": point.y,
+            "longitude": point.x,
         }
         return retail_store_dict
